c34d914107b334e4a4a68a38246498b05157280c
wgwntjd <k22675427@gmail.com>
2020-09-16 09:02:29 +0900

    no message

---
+++ Chap01/helloWorld2.js
@@ -0,0 +1,6 @@
+const http = require("http");
+
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end("<h1>Hello World!!</h1>");
+}).listen(3000);
